d639c947b701ed32bb740af33b93ad4b9e4ff140
Kumar Spandan Pattanayak <75578279+5p7Ro0t@users.noreply.github.com>
2022-05-05 20:44:57 +0530

    Update setdates.js

--- src/setdates.js
+++ src/setdates.js
@@ -2,18 +2,18 @@ var date = new Date();
 var day = date.getDay();
 var month = date.getMonth();
 switch(month){
-    case 1: month = "January"; break;
-    case 2: month = "February"; break;
-    case 3: month = "March"; break;
-    case 4: month = "April"; break;
-    case 5: month = "May"; break;
-    case 6: month = "June"; break;
-    case 7: month = "July"; break;
-    case 8: month = "August"; break;
-    case 9: month = "September"; break;
-    case 10: month = "October"; break;
-    case 11: month = "November"; break;
-    case 12: month = "December"; break;
+    case 0: month = "January"; break;
+    case 1: month = "February"; break;
+    case 2: month = "March"; break;
+    case 3: month = "April"; break;
+    case 4: month = "May"; break;
+    case 5: month = "June"; break;
+    case 6: month = "July"; break;
+    case 7: month = "August"; break;
+    case 8: month = "September"; break;
+    case 9: month = "October"; break;
+    case 10: month = "November"; break;
+    case 11: month = "December"; break;
 }
 switch(day){
     case 0: day = "Sunday"; break;
